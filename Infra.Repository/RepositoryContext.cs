388f980f35063bff57e610b59384bccdbc9e6ad7
Fructuoso <victor@fructuoso.com.br>
2020-12-11 12:19:25 -0300

    Configurando EF CodeFirst

--- Infra.Repository/RepositoryContext.cs
+++ Infra.Repository/RepositoryContext.cs
@@ -5,7 +5,10 @@ namespace DesignPatternSamples.Infra.Repository
     public class RepositoryContext : DbContext
     {
         public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options) { }
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
---
+++ Infra.Repository/RepositoryContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DesignPatternSamples.Infra.Repository
+{
+    public class BloggingContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<RepositoryContext>();
+            optionsBuilder.UseSqlServer(@"(localdb)\MSSQLLocalDB");
+
+            return new RepositoryContext(optionsBuilder.Options);
+        }
+    }
+}
